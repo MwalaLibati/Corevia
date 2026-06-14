d1aa34344128a42394fc70d1b2d2cf89b69a618c
Mwala Emmanuel Libati <emmanuel.libati@gmail.com>
2026-06-14 13:36:40 +0200

    Add production server config for Hostinger deploy

---
+++ config/server.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'app_secret' => '4MkN0jZSL1mpCIaJyfTdeiKw8sV5nRrtx9HXoquPYcD2gO6WhA3EGBvl7FUzQb',
+
+    'database' => [
+        'host' => '127.0.0.1',
+        'name' => 'u116180545_payroll',
+        'user' => 'u116180545_root',
+        'pass' => 'FjC8TRDncb#n!3R',
+        'charset' => 'utf8mb4',
+    ],
+];
